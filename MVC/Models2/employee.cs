b4e5cc0214cc3d0e9627b12253900ca789531fc9
chris <chris@DEVBOX>
2017-04-19 00:18:47 -0700

    validation

---
+++ MVC/Models2/employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+
+namespace MVC.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee
+    {
+    }
+
+    public class EmployeeMetaData
+    {
+        [StringLength(10, MinimumLength = 5)]
+        [Required]
+        [Remote("IsUserNameAvailable", "Employees", ErrorMessage = "UserName already in use.")]
+        public string Name { get; set; }
+
+    }
+}
